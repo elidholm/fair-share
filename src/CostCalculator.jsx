68f7b6b7c9f4009dd0b9e8467cdc8b2a876ad2ca
Edvin Lidholm <edvin.lidholm@volvocars.com>
2024-11-11 16:48:40 +0100

    feat: add button to delete incomes and expenses

--- src/CostCalculator.jsx
+++ src/CostCalculator.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Delete } from "react-feather";
 import ToggleSwitch from "./ToggleSwitch.jsx";
 
 function CostCalculator() {
@@ -23,9 +24,14 @@ function CostCalculator() {
 
   function addIncome() {
     if (newIncome.trim() !== "") {
-      setIncomes([...incomes, { name: newIncome, amount: "" }]);
+      setIncomes(i => [...i, { name: newIncome, amount: "" }]);
       setNewIncome("");
     }
+  }
+
+  function deleteIncome(index) {
+    const updatedIncomes = incomes.filter((_, i) => i !== index);
+    setIncomes(updatedIncomes);
   }
 
   const [expenses, setExpenses] = useState([
@@ -43,6 +49,25 @@ function CostCalculator() {
     updatedExpenses[index].amount = value === "" ? "" : parseFloat(value);
     setExpenses(updatedExpenses);
   }
+
+  const [newExpense, setNewExpense] = useState("");
+
+  function handleNewExpenseChange(event) {
+    setNewExpense(event.target.value);
+  }
+
+  function addExpense() {
+    if (newExpense.trim() !== "") {
+      setExpenses(e => [...e, { name: newExpense, amount: "" }]);
+      setNewExpense("");
+    }
+  }
+
+  function deleteExpense(index) {
+    const updatedExpenses = expenses.filter((_, i) => i !== index);
+    setExpenses(updatedExpenses);
+  }
+
 
   const [showSplit, setShowSplit] = useState(false);
 
@@ -81,6 +106,7 @@ function CostCalculator() {
               value={income.amount === "" ? "" : income.amount}
               onChange={(event) => handleIncomeChange(event, index)}
             />kr
+            <button className="remove-button" onClick={() => deleteIncome(index)}><Delete /></button>
           </li>
         ))}
       </ul>
@@ -93,7 +119,6 @@ function CostCalculator() {
         />
         <button className="add-button" onClick={addIncome}>+</button>
       </div>
-
       <li>Total: {totalIncome} kr</li>
 
       <ToggleSwitch id="split-mode" checked={splitMode} onChange={onSplitModeChange} />
@@ -110,10 +135,20 @@ function CostCalculator() {
               value={expense.amount === "" ? "" : expense.amount}
               onChange={(event) => handleExpenseChange(event, index)}
             />kr
+            <button className="remove-button" onClick={() => deleteExpense(index)}><Delete /></button>
           </li>
         ))}
-        <li>Total: {totalExpenses} kr</li>
       </ul>
+      <div className="add-expense">
+        <input
+          type="text"
+          placeholder="Enter new expense..."
+          value={newExpense}
+          onChange={handleNewExpenseChange}
+        />
+        <button className="add-button" onClick={addExpense}>+</button>
+      </div>
+      <li>Total: {totalExpenses} kr</li>
 
       <button className="split-button" onClick={() => setShowSplit(true)}>Split Expenses</button>
 
