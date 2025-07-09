c79270331f1727f06d2ecbd6d20b902094701432
Edvin Lidholm <edvin.lidholm@volvocars.com>
2025-07-09 11:10:31 +0200

    refact: local linting/CI scripts

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -2,6 +2,23 @@
 
 set -euo pipefail
 
+echo "Linting files..."
+
+echo -e "\n****** YAML Linting ******"
+yamllint . && echo "YAML linting passed."
+
+echo -e "\n****** Shell Script Linting ******"
+pushd scripts
+shellcheck ./*.sh && echo "Shell script linting passed."
+popd
+
+echo -e "\n****** Frontend Linting ******"
 pushd frontend
 npm run lint
 popd
+
+echo -e "\n****** Dockerfile Linting ******"
+for file in ./**/*Dockerfile*; do
+  docker run --rm -i ghcr.io/hadolint/hadolint < "$file"
+done && echo "Dockerfile linting passed."
+
