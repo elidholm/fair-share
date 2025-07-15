aa5103ef0902c302952a2e0455e34ede563b8b39
Edvin Lidholm <edvin.lidholm@volvocars.com>
2025-07-15 13:31:47 +0200

    feat: add api route to handle user expenses data

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ import cookieParser from 'cookie-parser';
 import authRouter from './routes/auth.js';
 import healthRouter from './routes/health.js';
 import incomesRouter from './routes/incomes.js';
+import expensesRouter from './routes/expenses.js';
 
 const app = express();
 app.use(express.json());
@@ -12,6 +13,7 @@ app.use(cookieParser());
 app.use('/auth', authRouter);
 app.use('/health', healthRouter);
 app.use('/incomes', incomesRouter);
+app.use('/expenses', expensesRouter);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
