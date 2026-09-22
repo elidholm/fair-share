9f13166c3195ea03f625f64d781062e76fd818b1
Edvin Lidholm <edvin.lidholm@volvocars.com>
2026-09-22 15:15:06 +0200

    fix: correct name of ssh key secret

--- web/src/components/nav_bar/NavBar.jsx
+++ web/src/components/nav_bar/NavBar.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import DesktopNavigation from "./DesktopNavigation.jsx";
 import MobileNavigation from "./MobileNavigation.jsx";
 
 const NavBar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => setIsScrolled(window.scrollY > 0);
+    handleScroll();
+    window.addEventListener("scroll", handleScroll, { passive: true });
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <div className="NavBar">
+    <div className={`NavBar${isScrolled ? " is-scrolled" : ""}`}>
       <DesktopNavigation />
       <MobileNavigation />
     </div>
